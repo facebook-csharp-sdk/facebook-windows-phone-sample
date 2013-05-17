22acf67e11b28615902e70b07e08be1656fac949
Nicolas Bello Camilletti <nicolas.bello@southworks.net>
2013-05-17 17:37:45 -0300

    # Updated Scrumptious tutorial end solution

---
+++ src/Facebook.Scrumptious.WindowsPhone/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Facebook.Scrumptious.WindowsPhone.Resources;
+
+namespace Facebook.Scrumptious.WindowsPhone
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
